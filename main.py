57621a142f503be744a9841d7e422c7a945f6452
Ayrton Gomes <ayrton.gomes@sintaxy.com>
2025-12-07 13:38:28 -0100

    fix inference opcode

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, BackgroundTasks
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from m8_client import M8
 from typing import Any
@@ -139,4 +140,15 @@ async def reset_agent():
 
 if __name__ == "__main__":
     import uvicorn
+
+    origins = ["*"]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=False,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     uvicorn.run(app, host="0.0.0.0", port=8002)
