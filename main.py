851bc9a9bfb86de3e42f1e2097c4df34cc66209c
Ayrton Gomes <ayrton.gomes@sintaxy.com>
2025-12-07 20:57:42 -0100

    try the gpu

--- main.py
+++ main.py
@@ -163,24 +163,24 @@ async def stream_chat_llm(req: ChatRequest):
 
     stream Begining processing...
     stall 0.05
-    llm_instance <input> instname n_predict=25 temperature=0.1 force=true stream=true
+    llm_instance <input> instname n_predict=120 temperature=0.1 force=true stream=true
     llm_instancestatus instname <r3_out>
     # stream Response1 IS DONE.
 
     # store <r3_out> PreviousAnswer: <r3_out>
-    llm_instance <r3_out> instname2 n_predict=55 temperature=0.1 force=true stream=true
+    llm_instance <r3_out> instname2 n_predict=155 temperature=0.1 force=true stream=true
     llm_instancestatus instname2 <r3_out>
     # stream Response 2 IS DONE.
 
     # store <r3_out> PreviousAnswer: <r3_out>
-    llm_instance <r3_out> instname3 n_predict=45 temperature=0.1 force=true stream=true
+    llm_instance <r3_out> instname3 n_predict=55 temperature=0.1 force=true stream=true
     # stream Response 3 IS DONE.
 
     # store <r3_out> PreviousAnswer: <r3_out>
     llm_instance <r3_out> instname3 n_predict=45 temperature=0.1 force=true stream=true
     # stream Response 4 IS DONE.
-
     """
+
     return StreamingResponse(
         M8.StreamSession(AGENT_SESSION_ID, script),
         media_type="text/plain"
