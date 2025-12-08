db09275c952e9c7b8e3ceed9883e69c527075cc8
Ayrton Gomes <ayrton.gomes@sintaxy.com>
2025-12-07 23:26:32 -0100

    working on llm_openai testing

--- main.py
+++ main.py
@@ -197,7 +197,7 @@ async def stream_chat_tests(req: ChatRequest):
 
     stream Begining processing...
     stall 0.05
-    llm_openai <input> instname n_predict=-1 temperature=0.1 force=true stream=true
+    llm_openai <input> instname n_predict=78 temperature=0.1 force=true stream=true
     llm_instancestatus instname <r3_out>
     """
     return StreamingResponse(
